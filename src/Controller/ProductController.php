5626b3b0e266c9778ed3364e53dff0e032c509d4
Lilyonix <k.boucry@gmail.com>
2024-07-09 19:47:36 +0200

    Init

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends AbstractController
+{
+    #[Route('/welcome', name: 'app_product_controler')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductController.php',
+        ]);
+    }
+
+    #[Route('/product', methods: 'GET')]
+    public function getProduct(ProductRepository $productRepository): JsonResponse
+    {
+        $product = $productRepository->findAll();
+        return $this->json($product);
+    }
+
+    #[Route('/product', methods: 'POST')]
+    public function createProduct(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $em->persist($product);
+        $em->flush();
+        return $this->json($product);
+    }
+}
